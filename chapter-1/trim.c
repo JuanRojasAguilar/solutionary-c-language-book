c9c02867ae837a2a1d4917075e05c51bf25eade7
Juan Rojas <yo217@hotmail.es>
2024-12-19 21:27:59 -0500

    file: :sparkles: exercise 1-18 trim whitespace

---
+++ chapter-1/trim.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAXLEN 100
+
+void get_word(char target[], int len);
+void trim(char word[], char target[]);
+
+int main(void)
+{
+  char line[MAXLEN];
+  char response[MAXLEN];
+
+  get_word(line, MAXLEN);
+  trim(line, response);
+
+  printf("%s\n", response);
+
+  return 0;
+}
+
+void get_word(char target[], int len) {
+  int c, idx;
+
+  for (idx = 0; idx < (len -1) && (c=getchar()) != EOF && c != '\n'; ++idx) {
+    target[idx] = c;
+  }
+
+  if (c == '\n') {
+    target[idx] = c;
+    ++idx;
+  }
+  target[idx] = '\0';
+}
+
+void trim(char word[], char res[]) {
+  int i, ri = 0;
+
+  while (word[i] != EOF) {
+    if (word[i] != ' ') {
+      res[ri] = word[i];
+      ++ri;
+    }
+    ++i;
+  }
+}
