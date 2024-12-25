0b3d80f548e7578ae36db467cf586f1332f4ca25
Juan Rojas <yo217@hotmail.es>
2024-12-25 17:18:18 -0500

    feat: :sparkles: trim refactored

--- chapter-1/trim/main.c
+++ chapter-1/trim/main.c
@@ -3,17 +3,18 @@
 #define MAXLEN 100
 
 void get_word(char target[]);
-void trim(char word[], char target[]);
+void trim(char word[]);
+void cpy_str(char dest[], char src[]);
+void add_end_chr(char dest[], int idx);
 
 int main(int argc, char **argv)
 {
   char line[MAXLEN];
-  char response[MAXLEN];
 
   get_word(line);
-  trim(line, response);
+  trim(line);
 
-  printf("%s\n", response);
+  printf("%s\n", line);
 
   return 0;
 }
@@ -25,19 +26,35 @@ void get_word(char target[]) {
     target[idx] = c;
   }
 
-  target[idx] = '\n';
-  ++idx;
-  target[idx] = '\0';
+  add_end_chr(target, idx);
 }
 
-void trim(char word[], char res[]) {
-  int i, ri = 0;
+void trim(char word[]) {
+  int i = 0, ri = 0;
+  char res[MAXLEN];
 
-  while (word[i] != EOF) {
+  while (word[i] != EOF && word[i] != '\n' && word[i] != '\0') {
     if (word[i] != ' ') {
       res[ri] = word[i];
       ++ri;
     }
     ++i;
   }
+  add_end_chr(res, ri);
+  cpy_str(word, res);
+}
+
+void cpy_str(char dest[], char src[]) {
+  int i = 0;
+  while (src[i] != '\0' && src[i] != '\n' && src[i] != EOF) {
+    dest[i] = src[i];
+    ++i;
+  }
+  add_end_chr(dest, i);
+}
+
+void add_end_chr(char dest[], int idx) {
+  dest[idx] = '\n';
+  ++idx;
+  dest[idx] = '\0';
 }
