bc8a54ae15efed8e60764ec8952af7b87b6176b2
Juan Rojas <yo217@hotmail.es>
2024-12-26 11:43:02 -0500

    fix: :ambulance: fixed trim logic, why would i check EOF everytime??

---
+++ chapter-1/reverse/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define MAXLEN 1000
+
+void get_line(char word[]);
+
+int main(void)
+{
+  char line[MAXLEN];
+
+  return 0;
+}
--- chapter-1/trim/main.c
+++ chapter-1/trim/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAXLEN 100
 
 void get_word(char target[]);
 void trim(char word[]);
-void cpy_str(char dest[], char src[]);
-void add_end_chr(char dest[], int idx);
 
 int main(int argc, char **argv)
 {
@@ -14,7 +13,7 @@ int main(int argc, char **argv)
   get_word(line);
   trim(line);
 
-  printf("%s\n", line);
+  puts(line);
 
   return 0;
 }
@@ -26,35 +25,24 @@ void get_word(char target[]) {
     target[idx] = c;
   }
 
-  add_end_chr(target, idx);
+  if (idx == MAXLEN - 1) {
+    fprintf(stderr, "Warning: Input stopped at %d characters", MAXLEN - 1);
+  }
+
+  target[idx] = '\0';
 }
 
 void trim(char word[]) {
   int i = 0, ri = 0;
   char res[MAXLEN];
 
-  while (word[i] != EOF && word[i] != '\n' && word[i] != '\0') {
-    if (word[i] != ' ') {
+  while (word[i] != '\0') {
+    if (word[i] != ' ' && word[i] != '\t') {
       res[ri] = word[i];
       ++ri;
     }
     ++i;
   }
-  add_end_chr(res, ri);
-  cpy_str(word, res);
-}
-
-void cpy_str(char dest[], char src[]) {
-  int i = 0;
-  while (src[i] != '\0' && src[i] != '\n' && src[i] != EOF) {
-    dest[i] = src[i];
-    ++i;
-  }
-  add_end_chr(dest, i);
-}
-
-void add_end_chr(char dest[], int idx) {
-  dest[idx] = '\n';
-  ++idx;
-  dest[idx] = '\0';
+  res[ri] = '\0';
+  strcpy(word, res);
 }
