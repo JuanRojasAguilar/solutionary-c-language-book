6be1a12d8b5aaae054cadb94c0c0708459d57091
Juan Rojas <yo217@hotmail.es>
2025-01-03 11:44:56 -0500

    feat: :sparkles: xor swap for reverse string ch-1

--- chapter-1/reverse/main.c
+++ chapter-1/reverse/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 #define MAXLEN 1000
 
@@ -16,29 +15,37 @@ int main(void)
   return 0;
 }
 
-void get_line(char word[]) {
-  int idx, c;
+void get_line(char word[])
+{
+  int idx;
+  int c;
 
-  for (idx = 0; idx < (MAXLEN - 1) && (c = getchar()) != EOF && c != '\n' && c != '\0'; ++idx) {
+  idx = 0;
+  while (idx < (MAXLEN - 1) && (c = getchar()) != EOF && c != '\n' && c != '\0')
+  {
     word[idx] = c;
+    ++idx;
   }
 
   word[idx] = '\0';
   reverse_line(word, idx);
 }
 
-void reverse_line(char word[], int count) {
-  int rev_idx = 0;
-  char rev[count];
+void reverse_line(char word[], int count)
+{
+  int idx;
 
   count -= 1;
 
-  while (count != -1) {
+  idx = 0;
+  while (idx <= (count >> 1))
+  {
+    printf("%s the letter, %d the try\n", &word[count - idx], idx);
+    word[idx] = word[count - idx] ^ word[idx];
+    word[count - idx] = word[idx] ^ word[count - idx];
+    word[idx] = word[count - idx] ^ word[idx];
 
-    rev[rev_idx] = word[count];
-    ++rev_idx;
-    --count;
+    ++idx;
   }
-  rev[rev_idx] = '\0';
-  strcpy(word, rev);
+  word[count + 1] = '\0';
 }
